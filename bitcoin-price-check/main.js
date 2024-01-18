0e4a6516bba518372b749abf4b354afe59ea06af
toro-martinez-alvaro <torom.alvaro@gmail.com>
2024-01-18 18:19:13 -0500

    Creation of Electron's main file.

---
+++ bitcoin-price-check/main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  //Selecting the index created after Angular compilation.
+  mainWindow.loadURL(`file://${__dirname}/dist/browser/index.html`);
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow();
+});
